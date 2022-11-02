a54d20d76c8ef9dc5099cf4ae5ccf63833cb9fe7
Pyll Gomez <57198612+workatease@users.noreply.github.com>
2022-11-02 01:05:18 -0400

    check access for the value

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Directus extension endpoint
 
-Endpoint to check if the collection, action and id/ids has the permision for the users role
+Endpoint to check if the collection, action and id/ids has the permission for the users role
 
 
-```/permissions/${collection}/${action}/${id}```
+```checkAccess/:collection/:action/:id```
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-import { defineEndpoint } from '@directus/extensions-sdk';
 
-export default defineEndpoint((router) => {
-	router.get('/permission/', 
-	(_req, res) => res.send('Hello, World!')
-	);
+/**
+ * @type {import('directus/').defineEndpoint}
+ *
+ */
+ import { defineEndpoint } from '@directus/extensions-sdk';
+export default defineEndpoint((router,{services,logger}) => {
+	const { AuthorizationService } = services;
+	router.get('/:collection/:action/:id', async (_req:any, res:any) => {
+		const { collection, action, id } = _req.params;
+		const options = { schema: _req.schema, accountability: _req.accountability };
+		try {
+			const authorizationService = new AuthorizationService(options);
+			await authorizationService.checkAccess(action,collection,id);
+			res.send(true);
+		} catch (error) {
+			logger.error(error);
+			res.send(false);
+		}
+	});
 });
